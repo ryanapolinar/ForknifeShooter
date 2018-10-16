1db5eaab7a160d508acf6a82bfef63911bb0e20a
ryanapolinar <apolinaryan@gmail.com>
2018-10-15 21:48:49 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        Vector2 direction = input.normalized;
+        Vector2 velocity = direction * speed;
+        Vector2 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+    }
+}
