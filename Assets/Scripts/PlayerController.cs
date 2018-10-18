d8c75ad752c730efc99969d3deef90f538e5477c
Minh Vu <minhhv1@uci.edu>
2018-10-17 18:47:11 -0700

    Made main player rigidbody 2d with responsive movement, also included sample assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 10f;
+    public float speed = 100f;
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rb = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
         Vector2 direction = input.normalized;
         Vector2 velocity = direction * speed;
         Vector2 moveAmount = velocity * Time.deltaTime;
 
         transform.Translate(moveAmount);
+        */
+        
+        //rigidbody
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 tempVect = new Vector2(h, v);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        transform.Translate(tempVect);
+        //rb.MovePosition(rb.position + (tempVect * speed) * Time.deltaTime);
     }
 }
