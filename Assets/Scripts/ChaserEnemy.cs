fccaa6c30b4260379bf1dc2f5d917e357332889d
ryanapolinar <apolinaryan@gmail.com>
2018-12-09 23:35:55 -0800

    Bully animations done

--- Assets/Scripts/ChaserEnemy.cs
+++ Assets/Scripts/ChaserEnemy.cs
@@ -9,10 +9,23 @@ public class ChaserEnemy : Enemy {
     public int chaseCooldownMax = 60;
     Vector2 movementVelocity;
 
+    public int shootCooldown = 0;
+    public int shootCooldownMax = 24;
+
+    public EnemyProjectile enemyProjectile;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     protected override void Start()
     {
         base.Start();
         health *= 2;
+
+        shootCooldown = shootCooldownMax;
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
@@ -22,12 +35,19 @@ public class ChaserEnemy : Enemy {
         {
             chaseCooldown--;
         }
+
+        // Update shootCooldown
+        if (shootCooldown > 0)
+        {
+            shootCooldown--;
+        }
     }
 
     void FixedUpdate()
     {
-        // MOVEMENT
+        // MOVEMENT AND SHOOTING
         Chase();
+        Shoot();
     }
 
     override protected void OnTriggerEnter2D(Collider2D collision)
@@ -45,8 +65,42 @@ public class ChaserEnemy : Enemy {
     {
         if (this.PlayerDetected() && chaseCooldown <= 0)
         {
+            // Run animation
+            animator.SetBool("bullyRunThrow", true);
+
             this.movementVelocity = this.DirectionToPlayer().normalized * speed;
             rb.MovePosition(rb.position + movementVelocity * Time.deltaTime);
+
+            // Set direction
+            if (movementVelocity.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            } else
+            {
+                spriteRenderer.flipX = true;
+            }
+        } else
+        {
+            // Idle animation
+            animator.SetBool("bullyRunThrow", false);
+        }
+    }
+
+    private void Shoot()
+    {
+        if (this.PlayerDetected() && shootCooldown <= 0)
+        {
+            // Generate a vector from the enemy to the player
+            Vector2 randomizer = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+            Vector2 projectileVector = (DirectionToPlayer() + randomizer).normalized * enemyProjectile.totalSpeed;
+
+            // Create the enemy projectile
+            EnemyProjectile newProjectile = Instantiate(enemyProjectile);
+            newProjectile.transform.position = this.transform.position;
+            newProjectile.setSpeed(projectileVector.x, projectileVector.y);
+
+            // Reset the cooldown
+            shootCooldown = shootCooldownMax;
         }
     }
 
---
+++ Assets/Scripts/ChaserProjectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserProjectile : EnemyProjectile {
+
+    override protected void Start()
+    {
+        base.Start();
+        totalSpeed = 6f;
+    }
+}
