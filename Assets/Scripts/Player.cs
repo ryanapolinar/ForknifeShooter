5be97521bd4ee8f415341d52f962df939a7ed885
ryanapolinar <apolinaryan@gmail.com>
2018-12-10 05:19:18 -0800

    Player animations done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : Unit {
 
     public float speed = 7f;
     float fireCooldown;
-    public float fireCooldownMax = .75f;
+    public float fireCooldownMax = .75f / 2;
     bool isDead = false;
 
     int invincibility = 0;
@@ -22,6 +22,9 @@ public class Player : Unit {
 
     // COMPONENTS
     PlayerController playerController;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
 
     // GAMEOBJECTS
     public Projectile projectile;
@@ -32,33 +35,73 @@ public class Player : Unit {
         base.Start();
         playerController = GetComponent<PlayerController>();
         fireCooldown = fireCooldownMax;
+        fireCooldownMax = .75f / 2;
         bigProjectile = false;
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         // UPDATE INVINCIBILITY
         if (invincibility > 0)
         {
             invincibility--;
-        } else
+        }
+        else
         {
             invincibility = 0;
-            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            spriteRenderer.color = Color.white;
         }
 
         if (!isDead)
         {
             // MOVEMENT
-            playerController.GetMovementVelocity(speed);
+            Vector2 mv = playerController.GetMovementVelocity(speed);
+
+            // Set player walking animation
+            if (mv.x != 0 || mv.y != 0)
+            {
+                animator.SetBool("playerWalk", true);
+            }
+            else
+            {
+                animator.SetBool("playerWalk", false);
+            }
+            if (mv.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (mv.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
 
             // FIRING
             fireCooldown = playerController.GetProjectileInput(projectile, fireCooldown, fireCooldownMax);
-        }
+            if (playerController.isFiring())
+            {
+                animator.SetBool("playerShoot", true);
 
+                if (playerController.facingRight())
+                {
+                    spriteRenderer.flipX = false;
+                }
+                else
+                {
+                    spriteRenderer.flipX = true;
+                }
+            }
+            else
+            {
+                animator.SetBool("playerShoot", false);
+            }
+
+        }
         // UI UPDATING
         healthText.text = "" + this.getHealth();
-        
     }
 
     override public void Damage(int damage)
@@ -77,9 +120,9 @@ public class Player : Unit {
         this.setInvincibility(this.invincibilityFrameMax);
 
         // Fade while invincible
-        Color fadedColor = gameObject.GetComponent<SpriteRenderer>().color;
+        Color fadedColor = spriteRenderer.color;
         fadedColor.a = 0.5f;
-        gameObject.GetComponent<SpriteRenderer>().color = fadedColor;
+        spriteRenderer.color = fadedColor;
 
         if (health <= 0)
         {
